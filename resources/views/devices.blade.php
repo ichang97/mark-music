c3adfa4a88d1b7fd240616331e80aeefe6cc42fb
MarkAyinomoto <wprakongchai@gmail.com>
2022-04-30 15:17:11 +0700

    fix condition for show available devices.

--- resources/views/devices.blade.php
+++ resources/views/devices.blade.php
@@ -1,15 +1,21 @@
-<div class="row">
+@if($devices['devices'] && count($devices['devices']) > 0)
+    <div class="row">
 
-    @foreach($devices['devices'] as $device)
-        <div class="col-md-6">
-            <div class="card shadow-sm">
-                <div class="card-body">
-                    <h6>Device name : {{$device['name']}}</h6>
+        @foreach($devices['devices'] as $device)
+            <div class="col-md-6">
+                <div class="card shadow-sm">
+                    <div class="card-body">
+                        <h6>Device name : {{$device['name']}}</h6>
 
-                    <button class="btn btn-success" data-action="play-track" data-uri="{{$devices['uri']}}" data-device="{{$device['id']}}">PLAY</button>
-                    <button class="btn btn-success d-none" data-action="switch-player" data-device="{{$device['id']}}">SWITCH PLAYER</button>
+                        <button class="btn btn-success" data-action="play-track" data-uri="{{$devices['uri']}}" data-device="{{$device['id']}}">PLAY</button>
+                        <button class="btn btn-success d-none" data-action="switch-player" data-device="{{$device['id']}}">SWITCH PLAYER</button>
+                    </div>
                 </div>
             </div>
-        </div>
-    @endforeach
-</div>
+        @endforeach
+    </div>
+@else
+    <div class="alert alert-warning">
+        No device available. Please open spotify on some device.
+    </div>
+@endif
